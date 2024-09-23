01e2a99be2bd9f00c327445a9b61d904a7b25474
yashptl421 <yashptl421@gmail.com>
2024-09-23 18:20:16 +0530

    Customer Service updated

--- customer_webservices/src/main/java/com/yash/banking/customer_webservices/controller/ConfigController.java
+++ customer_webservices/src/main/java/com/yash/banking/customer_webservices/controller/ConfigController.java
@@ -1,19 +1,18 @@
 package com.yash.banking.customer_webservices.controller;
 
+import com.yash.banking.customer_webservices.response.ConfigResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ConfigController {
-    @Value("${user.role}")
-    private String userRole;
 
-    @Value("${welcome.message}")
-    private String welcomeMessage;
-
+    @Autowired
+    ConfigResponse response;
     @GetMapping("/config")
     public String getConfig() {
-        return String.format("Role: %s, Message: %s", userRole, welcomeMessage);
+        return String.format("Role: %s, Message: %s", response.getRole(), response.getMessage());
     }
 }
--- customer_webservices/src/main/java/com/yash/banking/customer_webservices/response/ConfigResponse.java
+++ customer_webservices/src/main/java/com/yash/banking/customer_webservices/response/ConfigResponse.java
@@ -1,22 +1,19 @@
 package com.yash.banking.customer_webservices.response;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
+@ConfigurationProperties("customer")
 public class ConfigResponse {
-    @Value("${user.role}")
-    private String userRole;
+    @Setter
+    @Getter
+    private String role;
+    @Getter
+    @Setter
+    private String message;
 
-    @Value("${welcome.message}")
-    private String welcomeMessage;
-
-    // Getter methods
-    public String getUserRole() {
-        return userRole;
-    }
-
-    public String getWelcomeMessage() {
-        return welcomeMessage;
-    }
 }
